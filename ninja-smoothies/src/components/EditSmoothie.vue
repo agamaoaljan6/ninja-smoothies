114d67852b252d7a6c6b2773d485bb9431e119e1
Aljan Agamao <agamaoaljan@gmail.com>
2019-08-03 10:51:45 -0700

    queries from firestore to edit smoothie

--- ninja-smoothies/src/components/EditSmoothie.vue
+++ ninja-smoothies/src/components/EditSmoothie.vue
@@ -1,21 +1,32 @@
 <template>
-    <div class="edit-smoothie container">
-        <h2>Edit a Smoothie {{this.$route.params.smoothie_slug}}</h2>
-
-    </div>
+  <div v-if="smoothie" class="edit-smoothie container">
+    <h2>Edit {{smoothie.title}} Smoothie </h2>
+  </div>
 </template>
 
 <script>
-export default {
-    name: 'EditSmoothie',
-    data() {
-        return {
+import db from "@/firebase/init";
 
-        }
-    }
-}
+export default {
+  name: "EditSmoothie",
+  data() {
+    return {
+        smoothie: null,
+    };
+  },
+  created() {
+    let ref = db
+      .collection("smoothies")
+      .where("slug", "==", this.$route.params.smoothie_slug);
+    ref.get().then(snapshot => {
+      snapshot.forEach(doc => {
+          this.smoothie = doc.data()
+          this.smoothie.id = doc.id
+      });
+    });
+  }
+};
 </script>
 
 <style>
-
 </style>
