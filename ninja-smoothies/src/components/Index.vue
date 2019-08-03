30ad65aafbfc4f69fcdacabc58bada0b2f1b7be9
Aljan Agamao <agamaoaljan@gmail.com>
2019-08-02 17:07:31 -0700

    deleting smoothies from database

--- ninja-smoothies/src/components/Index.vue
+++ ninja-smoothies/src/components/Index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="index container">
-    <div class="card" v-for="(smoothie) in smoothies" :key='smoothie.id'>
+    <div class="card" v-for="(smoothie) in smoothies" :key="smoothie.id">
       <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
       <div class="card-content">
         <h2 class="indigo-text">{{smoothie.title}}</h2>
         <ul class="ingredients">
-          <li v-for="(ingredient, index) in smoothie.ingredients" :key='index'>
+          <li v-for="(ingredient, index) in smoothie.ingredients" :key="index">
             <span class="chip">{{ingredient}}</span>
           </li>
         </ul>
@@ -15,35 +15,42 @@
 </template>
 
 <script>
-import db from '@/firebase/init'
+import db from "@/firebase/init";
 
 export default {
-  name: 'Index',
-  data () {
+  name: "Index",
+  data() {
     return {
       smoothies: []
-    }
+    };
   },
   methods: {
     deleteSmoothie(id) {
-      this.smoothies = this.smoothies.filter(smoothie => {
-        return smoothie.id !== id 
-      })
+      // delete smoothies from firestore
+      db.collection("smoothies")
+        .doc(id)
+        .delete()
+        .then(() => {
+          this.smoothies = this.smoothies.filter(smoothie => {
+            return smoothie.id !== id;
+          }); 
+        });
     }
   },
   created() {
     // fetching data into the firestore
-    db.collection('smoothies').get()
-    .then(snapshot => {
-      snapshot.forEach(doc => {
-        // console.log(doc.data(), doc.id)
-        let smoothie = doc.data()
-        smoothie.id = doc.id
-        this.smoothies.push(smoothie) // pushes data into smoothies array
-      })
-    })
+    db.collection("smoothies")
+      .get()
+      .then(snapshot => {
+        snapshot.forEach(doc => {
+          // console.log(doc.data(), doc.id)
+          let smoothie = doc.data();
+          smoothie.id = doc.id;
+          this.smoothies.push(smoothie); // pushes data into smoothies array
+        });
+      });
   }
-}
+};
 </script>
 
 <style>
@@ -66,11 +73,10 @@ export default {
 }
 .index .delete {
   position: absolute;
-  top:4px;
+  top: 4px;
   right: 4px;
   cursor: pointer;
   color: #aaa;
   font-size: 1.4em;
 }
-
 </style>
